ac63fc9aa9ffbc11b26b4a457311bbd7e4a9c23b
jun-geun-choi <jklucky73@gmail.com>
2025-07-04 15:19:31 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return  "Hello, Spring application";
+	}
+	
+	
+}
